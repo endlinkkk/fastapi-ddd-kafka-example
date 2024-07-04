b50605220489338f2f122ed1e79a69f6578f224c
Sergey <one6sergey@gmail.com>
2024-07-04 11:01:29 +0300

    initial commit

---
+++ app/application/api/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+
+def create_app() -> FastAPI:
+    return FastAPI(
+        title='Simple Kafka Chat',
+        docs_url='/api/docs',
+        description='Simple Kafka + DDD example',
+        debug=True,
+    )
